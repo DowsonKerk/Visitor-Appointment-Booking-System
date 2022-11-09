f7e4d1d7a451c7b6348f9c7d0855197070926b13
Jybryll <jybryllwong@gmail.com>
2022-11-09 20:27:33 +0800

    Update productcatalogue.php

--- productcatalogue.php
+++ productcatalogue.php
@@ -77,13 +77,78 @@
 </nav>
 
 <br><br><br>
-<form  action="search.php" method="POST">
-<div class="input-group">
-    <div class="d-flex justify-content-center mx-auto">
-        <div class="form-outline" style="width: 80%;">
-            <input type="search" class="form-control" placeholder="Search">
+<div class="row col-12">  
+    <div class="row col-10 mx-auto">
+    <div class="row mx-auto my-auto">
+        <div class="col">
+            <h2 class="text-center">Product Catalogue</h2>
         </div>
-    <button type="button" class="btn btn-primary">Search</button>
+    </div>
+
+
+    <form  action="search.php" method="POST">
+        <div class="d-flex justify-content-center mx-auto">
+            <div class="form-outline" style="width: 50%;">
+                <input type="search" class="form-control" placeholder="Search">
+            </div>
+            <button type="button" class="btn btn-primary">Search</button>
+        </div>
+    </form>
+    <br><br><br>
+
+    <?php 
+        $query = "SELECT * FROM tblproductcatalogue";
+        $query_run = mysqli_query($con, $query);
+
+        if(mysqli_num_rows($query_run) > 0)
+        {
+            foreach($query_run as $productcatalogue)
+            {
+        ?>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-2">
+                        <img src="admin\uploadedimage\<?php echo $productcatalogue['stockPicture']?>" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    
+                    <div class="col-md-8 my-auto">
+                        <div class="card-body">
+                            <h5 class="card-title text-center"><?php echo $productcatalogue['stockName']?></h5>
+                            <p class="card-text d-flex align-items-center justify-content-center pt-5"><small class="text-muted">Click for more details</small></p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 my-auto">
+                        <div class="card-body">
+                            <p class="card-text text-center h2"><?php echo $productcatalogue['stockPrice']?>$</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+                }
+            }
+            ?>
     </div>
 </div>
-</form>
+
+<div class="container-fluid border" style="width: 100%;">
+  <footer class="py-1 my-2 ">
+  <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Product</a></li>
+      <!-- <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Booking</a></li> -->
+      <!-- <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Notification</a></li> -->
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Enquiry Page</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Customer Service</a></li>
+      <!-- <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Report</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Product Catalogue</a></li> -->
+    </ul>
+    <p class="text-center text-muted">© 2022 Cacti-Succulent Kuching</p>
+  </footer>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
