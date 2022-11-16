bb07854097e53762f2e5dd22bf173943196dfc9b
DowsonKerk <74602619+DowsonKerk@users.noreply.github.com>
2022-11-16 21:44:02 +0800

    Footer

--- login.php
+++ login.php
@@ -133,7 +133,6 @@
   <footer class="py-1 my-2 ">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="enquiryPage.php" class="nav-link px-2 text-muted">Enquiry Page</a></li>
     </ul>
     <p class="text-center text-muted">© 2022 Cacti-Succulent Kuching</p>
   </footer>
